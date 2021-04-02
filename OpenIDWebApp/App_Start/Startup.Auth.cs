3003aadc02621bae6d98ffe3e5f38ebfdfb4010e
MUIT-IAM <80740424+MUIT-IAM@users.noreply.github.com>
2021-04-02 14:16:25 +0700

    Add files via upload

---
+++ OpenIDWebApp/App_Start/Startup.Auth.cs
@@ -0,0 +1,52 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+using OpenIDWebApp.Class;
+
+namespace OpenIDWebApp
+{
+    public partial class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string clientSecret = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        private static string resource = ConfigurationManager.AppSettings["ida:ResourceID"];
+        private static string metadataAddress = ConfigurationManager.AppSettings["ida:ADFSDiscoveryDoc"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions() {
+                CookieManager = new SystemWebCookieManager()
+            });
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    ClientSecret = clientSecret,
+                    Resource = resource,
+                    Scope = "openid allatclaims",
+                    //Authority = authority,
+                    MetadataAddress = metadataAddress,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    RedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/?message=" + context.Exception.Message);
+                            //context.Response.Redirect("/Error?message=" + context.Response.StatusCode.ToString());
+                            return Task.FromResult(0);
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ OpenIDWebApp/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using OpenIDWebApp.Models;
+
+namespace OpenIDWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.IsAuthenticated)
+            {
+                UserProfile profile = new UserProfile(new System.Security.Claims.ClaimsIdentity(User.Identity));
+                Response.Write(Newtonsoft.Json.JsonConvert.SerializeObject(profile.Claims).Replace(",", ",<br />"));
+            }
+            else
+            {
+                if (!String.IsNullOrEmpty(Request.QueryString["message"]))
+                {
+                    Response.Write(Request.QueryString["message"]);
+                }
+                else
+                {
+                    HttpContext.Current.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectAuthenticationDefaults.AuthenticationType);
+                }
+            }
+        }
+    }
+}
